780c478e0bf9e48cb7e08f9fa49c896b866409d6
Sagar Hc <ssag07906@gmail.com>
2026-09-18 19:07:08 +0530

    starting with frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -124,10 +124,22 @@ async function logoutUserController(req,res){
 }
 /**
  * @name getMeController
- * @description get the current logged in user details
+ * @description get the current logged in user details.
  *@access private  
  */
-async function getMeController(req,res){}
+async function getMeController(req,res){
+
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message:"user data fetched successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+}
 
 
-module.exports ={registerUserController ,loginUserController ,logoutUserController} ;
+module.exports ={registerUserController ,loginUserController ,logoutUserController,getMeController} ;
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -34,6 +34,6 @@ authRouter.get("/logout",authController.logoutUserController);
  * @access private
  */
 
-authRouter.get("/get-me",authMiddleware.authUser,)
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
 
 module.exports = authRouter;
